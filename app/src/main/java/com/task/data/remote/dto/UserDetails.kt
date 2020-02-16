35d88fa8d2a6a14693ba1f32cce7cfa0e88d6529
ahmedeltaher <ahmedmahmoud.eltaher@gmail.com>
2020-02-16 10:29:11 +0100

    Fix Gson null data in userDetails

--- app/src/main/java/com/task/data/remote/dto/UserDetails.kt
+++ app/src/main/java/com/task/data/remote/dto/UserDetails.kt
@@ -1,57 +1,59 @@
 package com.task.data.remote.dto
+
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
+
 @Parcelize
 data class UserDetails(
-    @SerializedName("avatar_url")
-    var avatarUrl: String = "",
-    var bio: String = "",
-    var blog: String = "",
-    var company: String = "",
-    @SerializedName("created_at")
-    var createdAt: String = "",
-    var email: String = "",
-    @SerializedName("events_url")
-    var eventsUrl: String = "",
-    var followers: Int = 0,
-    @SerializedName("followers_url")
-    var followersUrl: String = "",
-    var following: Int = 0,
-    @SerializedName("following_url")
-    var followingUrl: String = "",
-    @SerializedName("gists_url")
-    var gistsUrl: String = "",
-    @SerializedName("gravatar_id")
-    var gravatarId: String = "",
-    var hireable: String = "",
-    @SerializedName("html_url")
-    var htmlUrl: String = "",
-    var id: Int = 0,
-    var location: String = "",
-    var login: String = "",
-    var name: String = "",
-    @SerializedName("node_id")
-    var nodeId: String = "",
-    @SerializedName("organizations_url")
-    var organizationsUrl: String = "",
-    @SerializedName("public_gists")
-    var publicGists: Int = 0,
-    @SerializedName("public_repos")
-    var publicRepos: Int = 0,
-    @SerializedName("received_events_url")
-    var receivedEventsUrl: String = "",
-    @SerializedName("repos_url")
-    var reposUrl: String = "",
-    @SerializedName("site_admin")
-    var siteAdmin: Boolean = false,
-    @SerializedName("starred_url")
-    var starredUrl: String = "",
-    @SerializedName("subscriptions_url")
-    var subscriptionsUrl: String = "",
-    var type: String = "",
-    @SerializedName("updated_at")
-    var updatedAt: String = "",
-    var url: String = ""
-):Parcelable
+        @SerializedName("avatar_url")
+        var avatarUrl: String? = "",
+        var bio: String?,
+        var blog: String?,
+        var company: String?,
+        @SerializedName("created_at")
+        var createdAt: String? = "",
+        var email: String?,
+        @SerializedName("events_url")
+        var eventsUrl: String? = "",
+        var followers: Int? = 0,
+        @SerializedName("followers_url")
+        var followersUrl: String? = "",
+        var following: Int? = 0,
+        @SerializedName("following_url")
+        var followingUrl: String? = "",
+        @SerializedName("gists_url")
+        var gistsUrl: String? = "",
+        @SerializedName("gravatar_id")
+        var gravatarId: String? = "",
+        var hireable: String?,
+        @SerializedName("html_url")
+        var htmlUrl: String? = "",
+        var id: Int? = 0,
+        var location: String? = "",
+        var login: String? = "",
+        var name: String? = "",
+        @SerializedName("node_id")
+        var nodeId: String? = "",
+        @SerializedName("organizations_url")
+        var organizationsUrl: String? = "",
+        @SerializedName("public_gists")
+        var publicGists: Int? = 0,
+        @SerializedName("public_repos")
+        var publicRepos: Int? = 0,
+        @SerializedName("received_events_url")
+        var receivedEventsUrl: String? = "",
+        @SerializedName("repos_url")
+        var reposUrl: String? = "",
+        @SerializedName("site_admin")
+        var siteAdmin: Boolean? = false,
+        @SerializedName("starred_url")
+        var starredUrl: String? = "",
+        @SerializedName("subscriptions_url")
+        var subscriptionsUrl: String? = "",
+        var type: String? = "",
+        @SerializedName("updated_at")
+        var updatedAt: String? = "",
+        var url: String? = ""
+) : Parcelable
